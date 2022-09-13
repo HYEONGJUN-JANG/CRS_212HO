000f585722d001c4ba7a884a862dd71f31df908f
hj <hjhj>
2022-09-13 18:29:47 +0900

    init_git_load.sh 을통한 data,model 다운

---
+++ init_git_load.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd data
+wget --no-check-certificate "https://hyu-my.sharepoint.com/:f:/g/personal/hirooms2_hanyang_ac_kr/EqhBl5fjWA9NqofHanHo4U8BZaVo2UUAyZQn_foycHYmrw?e=va13Pq&download=1"
+
+cd ../saved_model
+wget --no-check-certificate "https://hyu-my.sharepoint.com/:u:/g/personal/hirooms2_hanyang_ac_kr/Eawf83T57AlNh9ISAMA73PsB4MDJu6yb_nXNFMfHe8wfrw?e=E6ix9Y&download=1"
