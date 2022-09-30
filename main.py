df30f34b8b8ed15c21fa65c50a7e3f3f1cf9a370
melanie <yjw9700@agape.hanyang.ac.kr>
2022-09-30 16:39:45 +0900

    22.09.30 - combination 구현

--- main.py
+++ main.py
@@ -62,6 +62,19 @@ def randomize_model(model):
 
 if __name__ == '__main__':
 
+    # X = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    # Y = torch.tensor([[7, 8], [9, 10]])
+    # X1 = X.unsqueeze(0)
+    # Y1 = Y.unsqueeze(1)
+    # print(X1.shape, Y1.shape)
+    # X2 = X1.repeat(Y.shape[0], 1, 1)
+    # Y2 = Y1.repeat(1, X.shape[0], 1)
+    # print(X2.shape, X2.shape)
+    # Z = torch.cat([X2, Y2], -1)
+    # Z = Z.view(-1, Z.shape[-1])
+    # print(Z.shape)
+
+
     args = parse_args()
 
     pretrained_path = f'./saved_model/pretrained_model_{args.name}.pt'
