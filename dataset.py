aeeb015aa3546390eb7ff8d2dc10ff3ec8121bea
taeho <hirooms2@hanyang.ac.kr>
2022-10-02 15:07:03 +0900

    221002 Dataset Fix BERT layer freeze

--- dataset.py
+++ dataset.py
@@ -27,43 +27,43 @@ class ContentInformation(Dataset):
         self.read_data(tokenizer, args.max_plot_len, args.max_review_len)
         self.key_list = list(self.data_samples.keys())
 
-        self.entity2id = json.load(
-            open(os.path.join(self.data_path, 'entity2id.json'), 'r', encoding='utf-8'))  # {entity: entity_id}
-        self.id2entity = {idx: entity for entity, idx in self.entity2id.items()}
-        self.n_entity = max(self.entity2id.values()) + 1
-        # {head_entity_id: [(relation_id, tail_entity_id)]}
-        self.entity_kg = json.load(open(os.path.join(self.data_path, 'dbpedia_subkg.json'), 'r', encoding='utf-8'))
-        self.entity_kg = self._entity_kg_process()
-
-    def _entity_kg_process(self, SELF_LOOP_ID=185):
-        edge_list = []  # [(entity, entity, relation)]
-        for entity in range(self.n_entity):
-            if str(entity) not in self.entity_kg:
-                continue
-            edge_list.append((entity, entity, SELF_LOOP_ID))  # add self loop
-            for tail_and_relation in self.entity_kg[str(entity)]:
-                if entity != tail_and_relation[1] and tail_and_relation[0] != SELF_LOOP_ID:
-                    edge_list.append((entity, tail_and_relation[1], tail_and_relation[0]))
-                    edge_list.append((tail_and_relation[1], entity, tail_and_relation[0]))
-
-        relation_cnt, relation2id, edges, entities = defaultdict(int), dict(), set(), set()
-        for h, t, r in edge_list:
-            relation_cnt[r] += 1
-        for h, t, r in edge_list:
-            if relation_cnt[r] > 1000:
-                if r not in relation2id:
-                    relation2id[r] = len(relation2id)
-                edges.add((h, t, relation2id[r]))
-                entities.add(self.id2entity[h])
-                entities.add(self.id2entity[t])
-        return {
-            'edge': list(edges),
-            'n_relation': len(relation2id),
-            'entity': list(entities)
-        }
+    #     self.entity2id = json.load(
+    #         open(os.path.join(self.data_path, 'entity2id.json'), 'r', encoding='utf-8'))  # {entity: entity_id}
+    #     self.id2entity = {idx: entity for entity, idx in self.entity2id.items()}
+    #     self.n_entity = max(self.entity2id.values()) + 1
+    #     # {head_entity_id: [(relation_id, tail_entity_id)]}
+    #     self.entity_kg = json.load(open(os.path.join(self.data_path, 'dbpedia_subkg.json'), 'r', encoding='utf-8'))
+    #     self.entity_kg = self._entity_kg_process()
+    #
+    # def _entity_kg_process(self, SELF_LOOP_ID=185):
+    #     edge_list = []  # [(entity, entity, relation)]
+    #     for entity in range(self.n_entity):
+    #         if str(entity) not in self.entity_kg:
+    #             continue
+    #         edge_list.append((entity, entity, SELF_LOOP_ID))  # add self loop
+    #         for tail_and_relation in self.entity_kg[str(entity)]:
+    #             if entity != tail_and_relation[1] and tail_and_relation[0] != SELF_LOOP_ID:
+    #                 edge_list.append((entity, tail_and_relation[1], tail_and_relation[0]))
+    #                 edge_list.append((tail_and_relation[1], entity, tail_and_relation[0]))
+    #
+    #     relation_cnt, relation2id, edges, entities = defaultdict(int), dict(), set(), set()
+    #     for h, t, r in edge_list:
+    #         relation_cnt[r] += 1
+    #     for h, t, r in edge_list:
+    #         if relation_cnt[r] > 1000:
+    #             if r not in relation2id:
+    #                 relation2id[r] = len(relation2id)
+    #             edges.add((h, t, relation2id[r]))
+    #             entities.add(self.id2entity[h])
+    #             entities.add(self.id2entity[t])
+    #     return {
+    #         'edge': list(edges),
+    #         'n_relation': len(relation2id),
+    #         'entity': list(entities)
+    #     }
 
     def read_data(self, tokenizer, max_plot_len, max_review_len):
-        f = open(os.path.join(self.data_path, '/content_data.json'), encoding='utf-8')
+        f = open(os.path.join(self.data_path, 'content_data.json'), encoding='utf-8')
 
         data = json.load(f)[0]
 
@@ -243,7 +243,7 @@ class ReDialDataset:
             f"[Load entity dictionary and KG from {os.path.join(self.data_path, 'entity2id.json')} and {os.path.join(self.data_path, 'dbpedia_subkg.json')}]")
 
         self.entity_kg = self._entity_kg_process()
-        self.content_dataset = ContentInformation(self.args, self.content_data_path, self.tokenizer,
+        self.content_dataset = ContentInformation(self.args, self.data_path, self.tokenizer,
                                                   self.args.device_id)
         logger.debug("[Finish entity KG process]")
 
