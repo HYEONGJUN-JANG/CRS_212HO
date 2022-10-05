99290f5ec4aeb4ad5759df35a610a1d52dfcf4ba
melanie <yjw9700@agape.hanyang.ac.kr>
2022-10-05 14:49:13 +0900

    2022.10.05 pretrain 때도 sampling

--- pretrain.py
+++ pretrain.py
@@ -23,30 +23,32 @@ def pretrain(args, model, pretrain_dataloader, path):
             optimizer.step()
         print('Loss:\t%.4f' % total_loss)
 
-        model.eval()
-        topk = [1, 5, 10, 20]
-        hit = [[], [], [], []]
-
-        for movie_id, meta, plot_token, plot_mask, review_token, review_mask in tqdm(
-                pretrain_dataloader, bar_format=' {percentage:3.0f} % | {bar:23} {r_bar}'):
-            scores, target_id = model.pre_forward(meta, plot_token, plot_mask, review_token, review_mask, movie_id,
-                                                  compute_score=True)
-            scores = scores[:, torch.LongTensor(model.movie2ids)]
-
-            # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
-            target_id = target_id.cpu().numpy()
-
-            for k in range(len(topk)):
-                sub_scores = scores.topk(topk[k])[1]
-                sub_scores = sub_scores.cpu().numpy()
-
-                for (label, score) in zip(target_id, sub_scores):
-                    y = model.movie2ids.index(label)
-                    hit[k].append(np.isin(y, score))
-
-        print('Epoch %d : pre-train test done' % (epoch + 1))
-        for k in range(len(topk)):
-            hit_score = np.mean(hit[k])
-            print('[pre-train] hit@%d:\t%.4f' % (topk[k], hit_score))
-
     torch.save(model.state_dict(), path)  # TIME_MODELNAME 형식
+
+    model.eval()
+    topk = [1, 5, 10, 20]
+    hit = [[], [], [], []]
+
+    for movie_id, meta, plot_token, plot_mask, review_token, review_mask in tqdm(
+            pretrain_dataloader, bar_format=' {percentage:3.0f} % | {bar:23} {r_bar}'):
+        scores, target_id = model.pre_forward(meta, plot_token, plot_mask, review_token, review_mask, movie_id,
+                                              compute_score=True)
+        scores = scores[:, torch.LongTensor(model.movie2ids)]
+
+        # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
+        target_id = target_id.cpu().numpy()
+
+        for k in range(len(topk)):
+            sub_scores = scores.topk(topk[k])[1]
+            sub_scores = sub_scores.cpu().numpy()
+
+            for (label, score) in zip(target_id, sub_scores):
+                y = model.movie2ids.index(label)
+                hit[k].append(np.isin(y, score))
+
+    print('Epoch %d : pre-train test done' % (epoch + 1))
+    for k in range(len(topk)):
+        hit_score = np.mean(hit[k])
+        print('[pre-train] hit@%d:\t%.4f' % (topk[k], hit_score))
+
+
