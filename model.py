b4d2535035cf1cdb80b5b3cd32cc3f4134df4a3b
taeho <hirooms2@hanyang.ac.kr>
2022-10-07 00:25:42 +0900

    TH 221006 Pos emb + linear transform first + token pos (fix)

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ class MovieExpertCRS(nn.Module):
         self.edge_type = self.edge_type.to(self.device_id)
         self.pad_entity_idx = 0
 
-        self.entity_attention = AdditiveAttention(self.kg_emb_dim, self.kg_emb_dim)
+        self.entity_attention = SelfDotAttention(self.kg_emb_dim, self.kg_emb_dim)
         # Dialog
         self.token_emb_dim = token_emb_dim
 
@@ -60,7 +60,7 @@ class MovieExpertCRS(nn.Module):
                 self.args.n_positions
             )
 
-        self.token_attention = AdditiveAttention(self.kg_emb_dim, self.kg_emb_dim)
+        self.token_attention = SelfDotAttention(self.kg_emb_dim, self.kg_emb_dim)
         self.linear_transformation = nn.Linear(self.token_emb_dim, self.kg_emb_dim)
 
         # Gating
