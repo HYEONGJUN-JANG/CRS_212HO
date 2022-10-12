39f9c544fa870e4adfea543ab8ede745bad36f9b
melanie <yjw9700@agape.hanyang.ac.kr>
2022-10-12 12:37:05 +0900

    2022-10-12 resultfile 생성 테스트2

--- tester.py
+++ tester.py
@@ -6,23 +6,25 @@ from parameters import parse_args
 from main import main
 import numpy as np
 
-NUM_TRIAL = 3
+NUM_TRIAL = 1
 all_results = []
 if __name__ == '__main__':
     args = parse_args()
     command = 'python main.py'
     for i, v in vars(args).items():
-        print(i, v)
+        # print(i, v)
         command += f' --{i}={v}'
-    num_metric = 0
+    # num_metric = 0
     for _ in range(NUM_TRIAL):
         result = main(args)
         all_results.append(result)
     all_results = np.array(all_results)
+    print(all_results)
     avg_result = np.mean(all_results, axis=0)
+    print(avg_result)
 
     mdhm = str(datetime.now(timezone('Asia/Seoul')).strftime('%m%d%H%M%S'))  # MonthDailyHourMinute .....e.g., 05091040
     results_file_path = f"./results/Final_{mdhm}_name_{args.name}.txt"
     # parameters
-    with open(results_file_path, 'a', encoding='utf-8') as result_f:
+    with open(results_file_path, 'w', encoding='utf-8') as result_f:
         result_f.write('\t'.join(list(avg_result)))
--- train.py
+++ train.py
@@ -14,74 +14,74 @@ def train_recommender(args, model, train_dataloader, test_dataloader, path, resu
     for epoch in range(args.epoch):
 
         model.eval()
-        topk = [1, 5, 10, 20, 50]
-        hit_ft = [[], [], [], [], []]
-        hit_pt = [[], [], [], [], []]
-
-        if 'none' not in args.name:
-            # Pre-training Test
-            for movie_id, plot_meta, plot_token, plot_mask, review_meta, review_token, review_mask in tqdm(
-                    pretrain_dataloader):
-                scores, target_id = model.pre_forward(plot_meta, plot_token, plot_mask, review_meta, review_token,
-                                                      review_mask, movie_id, compute_score=True)
-                scores = scores[:, torch.LongTensor(model.movie2ids)]
-
-                # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
-                target_id = target_id.cpu().numpy()
-
-                for k in range(len(topk)):
-                    sub_scores = scores.topk(topk[k])[1]
-                    sub_scores = sub_scores.cpu().numpy()
-
-                    for (label, score) in zip(target_id, sub_scores):
-                        y = model.movie2ids.index(label)
-                        hit_pt[k].append(np.isin(y, score))
-
-            print('Epoch %d : pre-train test done' % (epoch))
-            for k in range(len(topk)):
-                hit_score = np.mean(hit_pt[k])
-                print('[pre-train] hit@%d:\t%.4f' % (topk[k], hit_score))
-
-            with open(results_file_path, 'a', encoding='utf-8') as result_f:
-                result_f.write(
-                    '[PRE TRAINING] Epoch:\t%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n' % (
-                        epoch, 100 * np.mean(hit_pt[0]), 100 * np.mean(hit_pt[1]), 100 * np.mean(hit_pt[2]),
-                        100 * np.mean(hit_pt[3]), 100 * np.mean(hit_pt[4])))
-
-        # Fine-tuning Test
-        for batch in test_dataloader.get_rec_data(args.batch_size, shuffle=False):
-            context_entities, context_tokens, _, _, _, _, _, _, target_items = batch
-
-            scores = model.forward(context_entities, context_tokens)
-
-            # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
-            scores = scores[:, torch.LongTensor(model.movie2ids)]
-            target_items = target_items.cpu().numpy()
-
-            for k in range(len(topk)):
-                sub_scores = scores.topk(topk[k])[1]
-                sub_scores = sub_scores.cpu().numpy()
-
-                for (label, score) in zip(target_items, sub_scores):
-                    target_idx = model.movie2ids.index(label)
-                    hit_ft[k].append(np.isin(target_idx, score))
-
-        print('Epoch %d : test done' % (epoch))
-
-        for k in range(len(topk)):
-            hit_score = np.mean(hit_ft[k])
-            print('hit@%d:\t%.4f' % (topk[k], hit_score))
-
-        with open(results_file_path, 'a', encoding='utf-8') as result_f:
-            result_f.write(
-                '[FINE TUNING] Epoch:\t%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n' % (
-                    epoch, 100 * np.mean(hit_ft[0]), 100 * np.mean(hit_ft[1]), 100 * np.mean(hit_ft[2]),
-                    100 * np.mean(hit_ft[3]), 100 * np.mean(hit_ft[4])))
-
-        if np.mean(hit_ft[0]) > eval_metric:
-            eval_metric = np.mean(hit_ft[0])
-            for k in range(len(topk)):
-                best_hit[k] = np.mean(hit_ft[k])
+        # topk = [1, 5, 10, 20, 50]
+        # hit_ft = [[], [], [], [], []]
+        # hit_pt = [[], [], [], [], []]
+        #
+        # if 'none' not in args.name:
+        #     # Pre-training Test
+        #     for movie_id, plot_meta, plot_token, plot_mask, review_meta, review_token, review_mask in tqdm(
+        #             pretrain_dataloader):
+        #         scores, target_id = model.pre_forward(plot_meta, plot_token, plot_mask, review_meta, review_token,
+        #                                               review_mask, movie_id, compute_score=True)
+        #         scores = scores[:, torch.LongTensor(model.movie2ids)]
+        #
+        #         # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
+        #         target_id = target_id.cpu().numpy()
+        #
+        #         for k in range(len(topk)):
+        #             sub_scores = scores.topk(topk[k])[1]
+        #             sub_scores = sub_scores.cpu().numpy()
+        #
+        #             for (label, score) in zip(target_id, sub_scores):
+        #                 y = model.movie2ids.index(label)
+        #                 hit_pt[k].append(np.isin(y, score))
+        #
+        #     print('Epoch %d : pre-train test done' % (epoch))
+        #     for k in range(len(topk)):
+        #         hit_score = np.mean(hit_pt[k])
+        #         print('[pre-train] hit@%d:\t%.4f' % (topk[k], hit_score))
+        #
+        #     with open(results_file_path, 'a', encoding='utf-8') as result_f:
+        #         result_f.write(
+        #             '[PRE TRAINING] Epoch:\t%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n' % (
+        #                 epoch, 100 * np.mean(hit_pt[0]), 100 * np.mean(hit_pt[1]), 100 * np.mean(hit_pt[2]),
+        #                 100 * np.mean(hit_pt[3]), 100 * np.mean(hit_pt[4])))
+        #
+        # # Fine-tuning Test
+        # for batch in test_dataloader.get_rec_data(args.batch_size, shuffle=False):
+        #     context_entities, context_tokens, _, _, _, _, _, _, target_items = batch
+        #
+        #     scores = model.forward(context_entities, context_tokens)
+        #
+        #     # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
+        #     scores = scores[:, torch.LongTensor(model.movie2ids)]
+        #     target_items = target_items.cpu().numpy()
+        #
+        #     for k in range(len(topk)):
+        #         sub_scores = scores.topk(topk[k])[1]
+        #         sub_scores = sub_scores.cpu().numpy()
+        #
+        #         for (label, score) in zip(target_items, sub_scores):
+        #             target_idx = model.movie2ids.index(label)
+        #             hit_ft[k].append(np.isin(target_idx, score))
+        #
+        # print('Epoch %d : test done' % (epoch))
+        #
+        # for k in range(len(topk)):
+        #     hit_score = np.mean(hit_ft[k])
+        #     print('hit@%d:\t%.4f' % (topk[k], hit_score))
+        #
+        # with open(results_file_path, 'a', encoding='utf-8') as result_f:
+        #     result_f.write(
+        #         '[FINE TUNING] Epoch:\t%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n' % (
+        #             epoch, 100 * np.mean(hit_ft[0]), 100 * np.mean(hit_ft[1]), 100 * np.mean(hit_ft[2]),
+        #             100 * np.mean(hit_ft[3]), 100 * np.mean(hit_ft[4])))
+        #
+        # if np.mean(hit_ft[0]) > eval_metric:
+        #     eval_metric = np.mean(hit_ft[0])
+        #     for k in range(len(topk)):
+        #         best_hit[k] = np.mean(hit_ft[k])
 
         # TRAIN
         # model.train()
@@ -114,36 +114,36 @@ def train_recommender(args, model, train_dataloader, test_dataloader, path, resu
     hit_ft = [[], [], [], [], []]
     hit_pt = [[], [], [], [], []]
 
-    if 'none' not in args.name:
-        # Pre-training Test
-        for movie_id, plot_meta, plot_token, plot_mask, review_meta, review_token, review_mask in tqdm(
-                pretrain_dataloader):
-            scores, target_id = model.pre_forward(plot_meta, plot_token, plot_mask, review_meta, review_token,
-                                                  review_mask, movie_id,
-                                                  compute_score=True)
-            scores = scores[:, torch.LongTensor(model.movie2ids)]
-
-            # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
-            target_id = target_id.cpu().numpy()
-
-            for k in range(len(topk)):
-                sub_scores = scores.topk(topk[k])[1]
-                sub_scores = sub_scores.cpu().numpy()
-
-                for (label, score) in zip(target_id, sub_scores):
-                    y = model.movie2ids.index(label)
-                    hit_pt[k].append(np.isin(y, score))
-
-        print('Epoch %d : pre-train test done' % (args.epoch))
-        for k in range(len(topk)):
-            hit_score = np.mean(hit_pt[k])
-            print('[pre-train] hit@%d:\t%.4f' % (topk[k], hit_score))
-
-        with open(results_file_path, 'a', encoding='utf-8') as result_f:
-            result_f.write(
-                '[PRE TRAINING] Epoch:\t%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n' % (
-                    args.epoch, 100 * np.mean(hit_pt[0]), 100 * np.mean(hit_pt[1]), 100 * np.mean(hit_pt[2]),
-                    100 * np.mean(hit_pt[3]), 100 * np.mean(hit_pt[4])))
+    # if 'none' not in args.name:
+    #     # Pre-training Test
+    #     for movie_id, plot_meta, plot_token, plot_mask, review_meta, review_token, review_mask in tqdm(
+    #             pretrain_dataloader):
+    #         scores, target_id = model.pre_forward(plot_meta, plot_token, plot_mask, review_meta, review_token,
+    #                                               review_mask, movie_id,
+    #                                               compute_score=True)
+    #         scores = scores[:, torch.LongTensor(model.movie2ids)]
+    #
+    #         # Item에 해당하는 것만 score 추출 (실험: 학습할 때도 똑같이 해줘야 할 지?)
+    #         target_id = target_id.cpu().numpy()
+    #
+    #         for k in range(len(topk)):
+    #             sub_scores = scores.topk(topk[k])[1]
+    #             sub_scores = sub_scores.cpu().numpy()
+    #
+    #             for (label, score) in zip(target_id, sub_scores):
+    #                 y = model.movie2ids.index(label)
+    #                 hit_pt[k].append(np.isin(y, score))
+    #
+    #     print('Epoch %d : pre-train test done' % (args.epoch))
+    #     for k in range(len(topk)):
+    #         hit_score = np.mean(hit_pt[k])
+    #         print('[pre-train] hit@%d:\t%.4f' % (topk[k], hit_score))
+    #
+    #     with open(results_file_path, 'a', encoding='utf-8') as result_f:
+    #         result_f.write(
+    #             '[PRE TRAINING] Epoch:\t%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n' % (
+    #                 args.epoch, 100 * np.mean(hit_pt[0]), 100 * np.mean(hit_pt[1]), 100 * np.mean(hit_pt[2]),
+    #                 100 * np.mean(hit_pt[3]), 100 * np.mean(hit_pt[4])))
 
     # Fine-tuning Test
     for batch in test_dataloader.get_rec_data(args.batch_size, shuffle=False):
