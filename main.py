7d09b21f93590b014d6459f7d1969957af2a56cd
melanie <yjw9700@agape.hanyang.ac.kr>
2022-10-12 13:16:36 +0900

    2022-10-12 resultfile 생성 테스트_final_final

--- main.py
+++ main.py
@@ -111,14 +111,14 @@ def main(args):
 
     pretrain_dataloader = DataLoader(content_dataset, batch_size=args.batch_size, shuffle=True)
 
-    # # For pre-training
-    # if args.name != "none":
-    #     if not args.pretrained:
-    #         # content_data_path = REDIAL_DATASET_PATH + '/content_data.json'
-    #
-    #         pretrain(args, model, pretrain_dataloader, pretrained_path)
-    #     else:
-    #         model.load_state_dict(torch.load(pretrained_path))  # state_dict를 불러 온 후, 모델에 저장`
+    # For pre-training
+    if args.name != "none":
+        if not args.pretrained:
+            # content_data_path = REDIAL_DATASET_PATH + '/content_data.json'
+
+            pretrain(args, model, pretrain_dataloader, pretrained_path)
+        else:
+            model.load_state_dict(torch.load(pretrained_path))  # state_dict를 불러 온 후, 모델에 저장`
 
     train_dataloader = ReDialDataLoader(train_data, args.n_sample, word_truncate=args.max_dialog_len)
     test_dataloader = ReDialDataLoader(test_data, args.n_sample, word_truncate=args.max_dialog_len)
