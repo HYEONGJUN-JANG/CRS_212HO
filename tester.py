6433ce6f93985488bdadbb6b13a96894d71fddf9
taeho <hirooms2@hanyang.ac.kr>
2022-10-13 16:44:13 +0900

    2022.10.13 TH. REFACTORING 2

--- tester.py
+++ tester.py
@@ -6,23 +6,42 @@ from parameters import parse_args
 from main import main
 import numpy as np
 
-NUM_TRIAL = 3
-all_results = []
+NUM_TRIAL = 5
+content_hits, initial_hits, best_results = [], [], []
 if __name__ == '__main__':
     args = parse_args()
     command = 'python main.py'
     for i, v in vars(args).items():
-        print(i, v)
+        # print(i, v)
         command += f' --{i}={v}'
-    num_metric = 0
+    # num_metric = 0
     for _ in range(NUM_TRIAL):
-        result = main(args)
-        all_results.append(result)
-    all_results = np.array(all_results)
-    avg_result = np.mean(all_results, axis=0)
+        content_hit, initial_hit, best_result = main(args)
+        content_hits.append(content_hit)
+        initial_hits.append(initial_hit)
+        best_results.append(best_result)
+
+    print(content_hits)
+
+    avg_content_hits = np.mean(np.array(content_hits), axis=0)
+    avg_initial_hits = np.mean(np.array(initial_hits), axis=0)
+    avg_best_results = np.mean(np.array(best_results), axis=0)
 
     mdhm = str(datetime.now(timezone('Asia/Seoul')).strftime('%m%d%H%M%S'))  # MonthDailyHourMinute .....e.g., 05091040
     results_file_path = f"./results/Final_{mdhm}_name_{args.name}.txt"
     # parameters
-    with open(results_file_path, 'a', encoding='utf-8') as result_f:
-        result_f.write('\t'.join(list(avg_result)))
+    with open(results_file_path, 'w', encoding='utf-8') as result_f:
+        result_f.write(
+            '\n=================================================\n')
+        for i, v in vars(args).items():
+            result_f.write(f'{i}:{v} || ')
+        result_f.write('\n')
+        for t in range(NUM_TRIAL):
+            result_f.write('#TRIAL:\t%d\n', t)
+            result_f.write('content_hits:\t' + '\t'.join(format(x, ".2f") for x in content_hits[t]) + '\n')
+            result_f.write('initial_hits:\t' + '\t'.join(format(x, ".2f") for x in initial_hits[t]) + '\n')
+            result_f.write('best_hits:\t' + '\t'.join(format(x, ".2f") for x in best_results[t]) + '\n')
+
+        result_f.write('content_hits:\t' + '\t'.join(format(x, ".2f") for x in avg_content_hits) + '\n')
+        result_f.write('initial_hits:\t' + '\t'.join(format(x, ".2f") for x in avg_initial_hits) + '\n')
+        result_f.write('best_hits:\t' + '\t'.join(format(x, ".2f") for x in avg_best_results) + '\n')
