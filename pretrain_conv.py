1f4c93f8c56c88a6b2d8ab140586c4afd0718537
melanie <yjw9700@agape.hanyang.ac.kr>
2022-11-30 13:31:46 +0900

    2022-11-30 pretrain evaluate 수정

--- pretrain_conv.py
+++ pretrain_conv.py
@@ -9,7 +9,7 @@ from transformers import AutoTokenizer, AdamW, get_linear_schedule_with_warmup
 import json
 
 
-def evaluate(preds, tokenizer, log=False, log_file_path=None):
+def evaluate(input_ids, preds, tokenizer, log=False, log_file_path=None):
     log_file = open(log_file_path, 'w', buffering=1)
     # log_file.write(f'\n*** test-{epoch + 1} ***\n\n')
     decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=False)
@@ -17,10 +17,15 @@ def evaluate(preds, tokenizer, log=False, log_file_path=None):
                      decoded_preds]
     decoded_preds = [pred.strip() for pred in decoded_preds]
 
+    decoded_inputs = tokenizer.batch_decode(input_ids, skip_special_tokens=False)
+    decoded_inputs = [decoded_input.replace('<pad>', '').replace('<|endoftext|>', '') for decoded_input in
+                     decoded_inputs]
+    decoded_inputs = [input.strip() for input in decoded_inputs]
+
     if log:
-        for pred in decoded_preds:
+        for title, pred in zip(decoded_inputs, decoded_preds):
             log_file.write(json.dumps({
-                # 'context': context,
+                'title': title,
                 'pred': pred
             }, ensure_ascii=False) + '\n')
 
@@ -47,20 +52,20 @@ def pretrain_conv(args, gpt_model, tokenizer_gpt, pretrain_dataloader, pretrain_
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.conv_lr_pt)
     lr_scheduler = get_linear_schedule_with_warmup(optimizer, args.num_warmup_steps, max_train_steps)
 
-    for epoch in range(args.conv_epoch_pt):
-        logger.info(f'[Conv - Pre-training] Train-{epoch}')
-        gpt_model.train()
-        total_loss = 0
-        # train
-        for batch in tqdm(pretrain_dataloader, bar_format=' {percentage:3.0f} % | {bar:23} {r_bar}'):
-            loss = gpt_model(**batch['context'], labels=batch['response']).loss
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            lr_scheduler.step()
-            total_loss += loss.data.float()
-        print('[Epoch%d]\tLoss:\t%.4f' % (epoch, total_loss))
+    # for epoch in range(args.conv_epoch_pt):
+    #     logger.info(f'[Conv - Pre-training] Train-{epoch}')
+    #     gpt_model.train()
+    #     total_loss = 0
+    #     # train
+    #     for batch in tqdm(pretrain_dataloader, bar_format=' {percentage:3.0f} % | {bar:23} {r_bar}'):
+    #         loss = gpt_model(**batch['context'], labels=batch['response']).loss
+    #
+    #         optimizer.zero_grad()
+    #         loss.backward()
+    #         optimizer.step()
+    #         lr_scheduler.step()
+    #         total_loss += loss.data.float()
+    #     print('[Epoch%d]\tLoss:\t%.4f' % (epoch, total_loss))
 
     # test
     logger.info('[Conv - Pre-training] Test')
@@ -75,7 +80,7 @@ def pretrain_conv(args, gpt_model, tokenizer_gpt, pretrain_dataloader, pretrain_
         for gen_seq, length in zip(gen_seqs, batch['context_len']):
             gen_seq = [token_id for token_id in gen_seq if token_id != tokenizer_gpt.pad_token_id]
             gen_resp_ids.append(gen_seq[length:])
-        evaluate(gen_resp_ids, tokenizer_gpt,
+        evaluate(batch['context'].input_ids, gen_resp_ids, tokenizer_gpt,
                  log=True, log_file_path=path)
 
         if path is not None:
