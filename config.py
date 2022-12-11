440cdc9d8f141dcfa79671618020fed2b1b32e41
taeho <hirooms2@hanyang.ac.kr>
2022-12-11 16:19:18 +0900

    2022.12.11 [TH] No Encoder state Initial (movie_end 제거 conv_pretrain 뺴고)

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 bert_special_tokens_dict = {
-    'additional_special_tokens': ['<movie>', '<movieend>'],
+    'additional_special_tokens': ['<movie>'],
 }
-
+# todo: gpt2 에 <cls> 가 왜 필요?
 gpt2_special_tokens_dict = {
     'pad_token': '<pad>',
-    'cls_token': '<cls>',
-    'additional_special_tokens': ['<movie>', '<movieend>'],
+    # 'cls_token': '<cls>',
+    'additional_special_tokens': ['<movie>', '<explain>'],
 }
 
-prompt_special_tokens_dict = {
-    'additional_special_tokens': ['<movie>', '<movieend>'],
-}
+# prompt_special_tokens_dict = {
+#     'additional_special_tokens': ['<movie>', '<movieend>'],
+# }
--- dataset_conv.py
+++ dataset_conv.py
@@ -244,7 +244,7 @@ class CRSConvDataset(Dataset):
             # BERT_tokenzier 에 입력하기 위해 @IDX 를 해당 movie의 name으로 replace
             for idx, word in enumerate(utt['text']):
                 if word[0] == '@' and word[1:].isnumeric():
-                    utt['text'][idx] = '<movie> %s <movieend>' % self.movie2name[word[1:]][1]
+                    utt['text'][idx] = '<movie> %s' % self.movie2name[word[1:]][1]
 
             text = ' '.join(utt['text'])
             # text_token_ids = self.tokenizer(text, add_special_tokens=False).input_ids
